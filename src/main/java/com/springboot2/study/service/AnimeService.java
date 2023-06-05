5ec319e21fd0a279be21aa618d7792bed8e248e0
Jean Furman <jean_11spfc@hotmail.com>
2023-06-05 16:48:57 -0300

    Metodo save/post

--- src/main/java/com/springboot2/study/service/AnimeService.java
+++ src/main/java/com/springboot2/study/service/AnimeService.java
@@ -1,6 +1,8 @@
 package com.springboot2.study.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +14,13 @@ import com.springboot2.study.domain.Anime;
 @Service
 public class AnimeService {
 	
-	private List<Anime> animes = List.of(new Anime(1L, "Boku no hero"), new Anime(2L, "Berserk"));
+	private static List<Anime> animes; 
+		
+	static 
+	{
+		animes = new ArrayList<>(List.of(new Anime(1L, "Boku no hero"), new Anime(2L, "Berserk")));
+	}
+	
 	
 	public List<Anime> listAll(){
 		return animes;
@@ -23,6 +31,12 @@ public class AnimeService {
 				.filter(anime -> anime.getId().equals(id))
 				.findFirst()
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+	}
+
+	public Anime save(Anime anime) {
+		anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+		animes.add(anime);
+		return anime;
 	}
 
 }
